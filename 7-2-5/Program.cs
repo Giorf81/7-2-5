cc42004e20fe4210e2a931fc8dc62901a0c84727
Giorf81 <rejroberts7@gmail.com>
2024-11-09 14:53:51 +0300

    Добавьте файлы проекта.

---
+++ 7-2-5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _7_2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass obj = new DerivedClass();
+            obj.Display();
+        }
+    }
+    class Baseclass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+
+    class DerivedClass : Baseclass
+    {
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+    }
+}
